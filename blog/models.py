125c2ba988e5d4c7cbf7550e3e26b89d5b5d2993
mohammad <m.baniasadi.d@gmail.com>
2023-08-20 08:39:22 +0330

    create the Post and tag models and migrate them to the database and register them to admin.py

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import User
+import uuid
 
 # Create your models here.
+class Post(models.Model):
+    POBLISH_CH = (
+        ('P', 'Poblished'),
+        ('D', 'Draft')
+    )
+    title = models.CharField(max_length=200)
+    body = models.TextField()
+    writer = models.ForeignKey(User, on_delete=models.CASCADE)
+    status = models.CharField(max_length=1, choices=POBLISH_CH)
+    tags = models.ManyToManyField('Tag', null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.CharField(unique=True, primary_key=True, default=uuid.uuid4, editable=False, max_length=200)
+
+    def __str__(self):
+        return self.title
+    
+
+class Tag(models.Model):
+    name=models.CharField(max_length=120)
+
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.CharField(unique=True, primary_key=True, default=uuid.uuid4, editable=False, max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+    
