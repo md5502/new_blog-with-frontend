cd631e20c76a1f4f59524e0aff8f94a9a762eae2
mohammad <m.baniasadi.d@gmail.com>
2023-08-28 17:23:31 +0330

    add user app to setting and urls files

--- config/settings.py
+++ config/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
     # local apps
     'blog',
+    'user',
 ]
 
 MIDDLEWARE = [
--- config/urls.py
+++ config/urls.py
@@ -5,7 +5,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blog.urls'))
+    path('', include('blog.urls')),
+    path('', include('user.urls'))
 ] 
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
