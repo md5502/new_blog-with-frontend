6cd905cfee11448fe167a9cadb0be08ecff81591
mohammad <m.baniasadi.d@gmail.com>
2023-08-28 17:23:07 +0330

    change the migrations

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from user.models import UserProfile
 import uuid
 
 # Create your models here.
@@ -10,13 +11,21 @@ class Post(models.Model):
     )
     title = models.CharField(max_length=200)
     body = models.TextField()
-    writer = models.ForeignKey(User, on_delete=models.CASCADE)
+    owner = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
     status = models.CharField(max_length=1, choices=POBLISH_CH, default=POBLISH_CH[1][1])
-    tags = models.ManyToManyField('Tag')
+    tags = models.ManyToManyField('Tag', blank=True)
     image = models.ImageField(null=True, blank=True, upload_to='post-images', default='post-images/default.png')
 
     created = models.DateTimeField(auto_now_add=True)
     id = models.CharField(unique=True, primary_key=True, default=uuid.uuid4, editable=False, max_length=200)
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs) 
+        if not self.tags.exists():
+            uncategorized_tag, created = Tag.objects.get_or_create(name='Uncategorized')
+            print('\n\n\n ', uncategorized_tag, '\n\n\n ')
+            self.tags.add(uncategorized_tag)
+            print('\n\n\n ', uncategorized_tag, '\n\n\n ')
+
 
     def __str__(self):
         return self.title
